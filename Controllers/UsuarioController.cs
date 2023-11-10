d8397d72400858ab330974fdfe632ecd18fc47c0
William24A <willi241194@gmail.com>
2023-11-10 19:42:51 -0300

    commit dos

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using tl2_tp10_2023_William24A.Models;
+using RepoUsuarioU;
+
+namespace tl2_tp10_2023_William24A.Controllers;
+
+public class UsuarioController : Controller
+{
+    private readonly ILogger<UsuarioController> _logger;
+
+    private IDUsuarioRepository repoUsuarioC;
+
+    public UsuarioController(ILogger<UsuarioController> logger)
+    {
+        _logger = logger;
+        repoUsuarioC = new RepoUsuarioC();
+    }
+
+    [HttpGet]
+    public IActionResult Listar()
+    {
+        var usuarios = repoUsuarioC.GetAll();
+        return View("usuarios");
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
